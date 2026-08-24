4e2d7503fe485f3286ee1980fe12087d9debfe82
Aryan Dewangan <devcorp25@gmail.com>
2026-08-24 18:23:10 +0530

    Revert "Add SpotlightSection component with styles and data integration"

--- src/features/home/pages/HomePage.tsx
+++ src/features/home/pages/HomePage.tsx
@@ -1,7 +1,6 @@
 import CategorySection from "../components/CategorySection";
 import FeaturedProductsSection from "../components/FeaturedProductsSection";
 import HeroSection from "../components/HeroSection";
-import SpotlightSection from "../components/SpotlightSection";
 
 const HomePage = () => {
   return (
@@ -9,7 +8,6 @@ const HomePage = () => {
       <HeroSection />
       <CategorySection />
       <FeaturedProductsSection />
-      <SpotlightSection />
     </>
   );
 };
